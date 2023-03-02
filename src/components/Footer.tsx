21bea842989a8397c887a7b7e56be26321c74de0
ClaytonDewey <clay@dryadmedia.com>
2023-03-02 15:18:58 -0600

    Add social icons and attribution to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+import { IconFacebook, IconInstagram, IconPinterest } from './Icons';
 
 type FooterProps = {};
 
+const StyledFooter = styled.footer`
+  .attribution {
+    font-size: 11px;
+    text-align: center;
+  }
+  .attribution a {
+    color: hsl(228, 45%, 44%);
+  }
+`;
+
 const Footer: React.FC<FooterProps> = () => {
-  return <footer>I'm a footer</footer>;
+  return (
+    <StyledFooter>
+      <div className='social-icons'>
+        <IconFacebook />
+        <IconPinterest />
+        <IconInstagram />
+      </div>
+      <div className='attribution'>
+        Challenge by{' '}
+        <a
+          href='https://www.frontendmentor.io?ref=challenge'
+          rel='noreferrer'
+          target='_blank'>
+          Frontend Mentor
+        </a>
+        . Coded by{' '}
+        <a href='https://www.claytondewey.io/' rel='noreferrer' target='_blank'>
+          Clayton Dewey
+        </a>
+        .
+      </div>
+    </StyledFooter>
+  );
 };
 export default Footer;
