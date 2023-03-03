0a083728357f6258aadbb5d41d75f5c0b0c0e0cf
Clayton Dewey <clay@dryadmedia.com>
2023-03-02 22:15:18 -0600

    Wrap elements in a styled container

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 import Count from './Count';
 
 type CounterProps = {};
 
+const StyledDiv = styled.div`
+  text-align: center;
+  perspective: 400px;
+  margin: 20px auto;
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+`;
+
 const Counter: React.FC<CounterProps> = () => {
   return (
-    <div>
+    <StyledDiv>
       <Count />
       <Count />
       <Count />
       <Count />
-    </div>
+    </StyledDiv>
   );
 };
 export default Counter;
