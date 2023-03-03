ac23e7744b29c4a66d04f8b4a26c18ab97aa1e03
Clayton Dewey <clay@dryadmedia.com>
2023-03-02 21:56:38 -0600

    Wrap in container to center on page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import GlobalStyle from '../globalStyles';
+import styled from 'styled-components';
 import Counter from './Counter';
 import Footer from './Footer';
 
 type AppProps = {};
 
+const StyledContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+
+  h1 {
+    text-transform: uppercase;
+    letter-spacing: 5px;
+    margin-bottom: 10rem;
+  }
+`;
+
 const App: React.FC<AppProps> = () => {
   return (
     <>
       <GlobalStyle />
-      <main>
-        <h1>We're Launching Soon</h1>
-        <Counter />
-      </main>
-      <Footer />
+      <StyledContainer>
+        <main>
+          <h1>We&rsquo;re Launching Soon</h1>
+          <Counter />
+        </main>
+        <Footer />
+      </StyledContainer>
     </>
   );
 };
